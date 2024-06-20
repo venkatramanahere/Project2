c3fc1754bd6cee932ab1b561fe0eb9470e527029
venkatramanahere <b.ramanareddy123@gmail.com>
2024-06-20 11:06:36 -0400

    Find the frequency of each roll value.

--- die_visual.py
+++ die_visual.py
@@ -5,8 +5,15 @@ die = Die()
 
 # Make some rolls, and store results in a list.
 results = []
-for roll_num in range(100):
+for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
-print(results)
+# Analyze the results.
+frequencies = []
+poss_results = range(1, die.num_sides + 1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)

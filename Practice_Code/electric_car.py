270bfeaff66bef4f58ce00cc2985c524db9775a3
venkatramanahere <b.ramanareddy123@gmail.com>
2024-06-20 18:05:43 -0400

    After adding Inheritance concepts.

---
+++ Practice_Code/electric_car.py
@@ -0,0 +1,62 @@
+class Car:
+    """A simple attempt to represent a car."""
+
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+    
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer.")
+
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
+
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+
+    def __init__(self, make, model, year):
+        """
+        Initialize attributes of the parent class.
+        Then initialize attributes specific to an electric car.
+        """
+        super().__init__(make, model, year)
+        self.battery_size = 40
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-KWh battery.")
+
+"""
+my_new_car = Car('Audi', 'A4', '2024')
+print(my_new_car.get_descriptive_name())
+
+my_new_car.update_odometer(23000)
+my_new_car.read_odometer()
+
+my_new_car.increment_odometer(500)
+my_new_car.read_odometer()
+"""
+
+my_leaf = ElectricCar('Nissan', 'Leaf', 2024)
+print(my_leaf.get_descriptive_name())
+my_leaf.describe_battery()

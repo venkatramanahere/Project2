6a19195b0bb50e5b09a4071eda897533e5bff45f
venkatramanahere <b.ramanareddy123@gmail.com>
2024-06-20 13:07:24 -0400

    With list comprehensions added.

--- dice_visual_d6d6d6.py
+++ dice_visual_d6d6d6.py
@@ -2,34 +2,31 @@ import plotly.express as px
 
 from die import Die
 
-# Create a D6 and a D10.
+# Create three D6 dice.
 die_1 = Die()
-die_2 = Die(10)
+die_2 = Die()
+die_3 = Die()
 
 # Make some rolls, and store results in a list.
 results = []
 for roll_num in range(50_000):
-    result = die_1.roll() + die_2.roll() 
+    result = die_1.roll() + die_2.roll() + die_3.roll()  
     results.append(result)
 
 # Analyze the results.
 frequencies = []
-max_result = die_1.num_sides + die_2.num_sides
-poss_results = range(2, max_result + 1)
+max_result = die_1.num_sides + die_2.num_sides + die_3.num_sides
+poss_results = range(3, max_result + 1)
 for value in poss_results:
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Visualize the results.
-title = "Results of rolling a D6 and D10 50,000 Times"
+title = "Results of rolling three D6 dice 50,000 Times"
 labels = {'x' : 'Result', 'y' : 'Frequency of Result'}
 fig = px.bar(x=poss_results, y=frequencies, title = title, labels = labels)
 
 # Further customize the chart.
 fig.update_layout(xaxis_dtick=1)
 
-# To display the chart on a web page.
-# fig.show()
-
-# To store the chart to a file.
-fig.write_html('dice_visual_d6d10.html')
+fig.show()
--- dice_visual_d8d8.py
+++ dice_visual_d8d8.py
@@ -2,43 +2,34 @@ import plotly.express as px
 
 from die import Die
 
-# Create two D6 dice.
-die_1 = Die()
-die_2 = Die()
+# Create two D8 dice.
+die_1 = Die(8)
+die_2 = Die(8)
 
 # Make some rolls, and store results in a list.
-
-"""
 results = []
-
-for roll_num in range(1000):
-    # Result when we add the numbers on two dice.
-    # result = die_1.roll() + die_2.roll() 
-
-    # Result when we multiply the numbers on two dice.
-    result = die_1.roll() * die_2.roll() 
+for roll_num in range(50_000):
+    result = die_1.roll() + die_2.roll() 
     results.append(result)
-"""
-results = [die_1.roll() + die_2.roll() for num in range(1000)]
 
 # Analyze the results.
-"""
 frequencies = []
 max_result = die_1.num_sides + die_2.num_sides
 poss_results = range(2, max_result + 1)
 for value in poss_results:
     frequency = results.count(value)
     frequencies.append(frequency)
-"""
-poss_results = range(2, die_1.num_sides + die_2.num_sides + 1)
-frequencies = [results.count(value) for value in poss_results]
 
 # Visualize the results.
-title = "Results of rolling two D6 1,000 Times"
+title = "Results of rolling tw0 D8 dice 50,000 Times"
 labels = {'x' : 'Result', 'y' : 'Frequency of Result'}
 fig = px.bar(x=poss_results, y=frequencies, title = title, labels = labels)
 
 # Further customize the chart.
 fig.update_layout(xaxis_dtick=1)
 
+# To display the chart on a web page.
 fig.show()
+
+# To store the chart to a file.
+# fig.write_html('dice_visual_d6d10.html')
